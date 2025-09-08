76316ec0512091e9a10dddabe62b31f3116c3a95
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2025-09-08 19:41:12 +0300

    Kaarlolle

---
+++ Assets/OmatAssetit/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public float turnSpeed = 10f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float turn = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+
+        transform.Translate(0, 0, move);
+        transform.Rotate(0, turn, 0);    
+    }
+}
