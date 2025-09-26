9de39256e3b1d901d5e82ef4dab62c52ef05a75a
Suvi Syrjäläinen <suvi.syrjalainen@gmail.com>
2025-09-26 23:06:30 +0300

    tekoälyauto liikkuu rataa ympäri

--- Assets/OmatAssetit/Scripts/AICar.cs
+++ Assets/OmatAssetit/Scripts/AICar.cs
@@ -19,10 +19,34 @@ public class AICar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //otetaan listan ensimmäinen kohdepiste
         Transform target = waypoints[currentWpIndex];
 
-        Vector3 targetXYZ = new Vector3(target.position.x, target.position.y, target.position.z);
-
+        //otetaan targetista XZ-vektori
+        Vector3 targetXZ = new Vector3(target.position.x, target.position.y, target.position.z);
         
+        Vector3 direction = (targetXZ - transform.position).normalized;
+
+        // Lasketaan käännös suuntaa kohti
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+
+        // Käännetään autoa pehmeästi kohti kohdetta
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, turnSpeed * Time.deltaTime);
+
+        // Liikutaan eteenpäin auton oman etuakselin suuntaan
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // Jos auto on tarpeeksi lähellä kohdepistettä (alle 2 yksikköä)
+        if (Vector3.Distance(transform.position, target.position) < 2f)
+        {
+            // Siirrytään seuraavaan reittipisteeseen (ja palataan alkuun kun loppu saavutetaan)
+            currentWpIndex = (currentWpIndex + 1) % waypoints.Length;
+        }
+
+
+
+
+
+
     }
 }
