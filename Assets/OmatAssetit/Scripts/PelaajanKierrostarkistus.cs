403c0678c5ae5dc6a96c2afe0fe86b37f02548f9
Suvi Syrjäläinen <suvi.syrjalainen@gmail.com>
2025-11-06 18:45:27 +0200

    voitto toimii

--- Assets/OmatAssetit/Scripts/PelaajanKierrostarkistus.cs
+++ Assets/OmatAssetit/Scripts/PelaajanKierrostarkistus.cs
@@ -9,14 +9,33 @@ public class PelaajanKierrostarkistus : MonoBehaviour
     private bool[] visited;
     private int visitedCount = 0;
 
+    
+    void Awake()
+    {
+        ResetLap();
+    }
+
+    public void ResetLap()
+    {
+        visited = new bool[checkpointCount];
+        visitedCount = 0;
+    }
+
+
     public void MarkVisited(int index)
     {
-        Debug.Log($"Olen mennyt juuri läpi portista: {index}");
+        //Debug.Log($"Olen mennyt juuri läpi portista: {index}");
         if (!visited[index])
         {
            visited[index] = true;
            visitedCount++;
-           Debug.Log($"Olen mennyt läpi yhteensä {visitedCount} portista.");
+           //Debug.Log($"Olen mennyt läpi yhteensä {visitedCount} portista.");
         }
+
+       
+        //Debug.Log($"Onko kaikista menty läpi: {AllVisitedThisLap}");
     }
+
+     // Tarkistaa, onko kaikki portit käyty läpi tällä kierroksella
+    public bool AllVisitedThisLap => visitedCount == checkpointCount;
 }
--- Assets/OmatAssetit/Scripts/Tuomari.cs
+++ Assets/OmatAssetit/Scripts/Tuomari.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using TMPro;
 
 public class Tuomari : MonoBehaviour
 {
+    private bool voittajaSelvilla = false;
+
+    public TMP_Text voittajaTeksti;
+
+    void Start()
+    {
+        voittajaTeksti.text = "";
+    }
+
     private void OnTriggerEnter(Collider car)
     {
+
         CarIdentify id = car.GetComponent<CarIdentify>();
         if (id == null)
         {
@@ -11,6 +22,26 @@ public class Tuomari : MonoBehaviour
         }
 
         string winnerName = id.displayName;
-        Debug.Log($"WINNER: {winnerName}");
+
+
+        if (id.kind == CarKind.Player)
+        {
+            var validator = car.GetComponentInParent<PelaajanKierrostarkistus>();
+            if (validator == null || !validator.AllVisitedThisLap)
+            {
+                Debug.Log("Pelaaja ylitti maalin, mutta kaikki checkpointit eivät ole kunnossa → ei voittoa.");
+                return;
+            }
+        }
+
+
+        if(!voittajaSelvilla){
+            Debug.Log($"WINNER: {winnerName}");
+            voittajaTeksti.color = new Color32(238, 201, 11, 255);
+            voittajaTeksti.text = $"Voittaja on {winnerName}!!!";
+            voittajaSelvilla = true;
+        }
+
+
     }
 }
